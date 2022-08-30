d2bf37666e829fb0fa4edc518fbea6a100e78ba5
Lucas Comamala <lucascomamala@gmail.com>
2022-08-30 12:12:45 -0600

    Revert "Make API Calls"
    This reverts commit 9d04e44efa782f574ddf2f185fee2416ef96bae4.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './style.css';
-import generateShows from './modules/shows.js';
-import generateActors from './modules/actors.js';
-import generateActresses from './modules/actresses.js';
-
-const shows = generateShows();
-const actors = generateActors();
-const actresses = generateActresses();
-
-console.log("x: ", shows);
-console.log("y: ", actors);
-console.log("z: ", actresses);
--- src/modules/makeAPICall.js
+++
@@ -1,9 +0,0 @@
-const makeAPICall = async (requestURL) => {
-  const request = new Request(requestURL);
-  const response = await fetch(request);
-  const scoresText = await response.text();
-  const result = await JSON.parse(scoresText);
-  return result;
-}
-
-export default makeAPICall;
