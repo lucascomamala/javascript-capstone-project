200c149463abb7f7e43e55241a4d1061a864dc4b
Lucas Comamala <lucascomamala@gmail.com>
2022-08-30 22:09:09 -0600

    Fixed API Fetching

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import './style.css';
 import generateShows from './modules/shows.js';
-import generateActors from './modules/actors.js';
-import generateActresses from './modules/actresses.js';
+import generatePeople from './modules/people.js';
+import renderNav from './modules/nav.js';
+import renderHome from './modules/home.js';
 
-const shows = generateShows();
-const actors = generateActors();
-const actresses = generateActresses();
 
-console.log('x: ', shows);
-console.log('y: ', actors);
-console.log('z: ', actresses);
+const createPage = async () => {
+  const showsTemp = await generateShows();
+  const people = await generatePeople();
+  const shows = showsTemp.slice(0, 25);
+  const actors = people.filter((person) => person.gender === "Male" && person.birthday !== null).slice(0, 25);
+  const actoras = people.filter((person) => person.gender === "Female" && person.birthday !== null).slice(0, 25);
 
-document.getElementById('app').innerHTML = 'shows';
+  renderNav();
+  renderHome(shows, 1, 2);
+
+  console.log('x: ', shows);
+  console.log('y: ', actors);
+  console.log('z: ', actoras);
+}
+
+createPage();
