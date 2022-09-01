31ebd2416986d56c4cdc3f95644263628a3550f1
Lucas Comamala <lucascomamala@gmail.com>
2022-09-01 12:40:09 -0600

    Add dynamic counters

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -7,13 +7,13 @@ const header = `
     <img src="" class="logo" alt="main logo" />
   </li>
   <li class="nav-item">
-    <a class="nav-link active" href="#">Shows <span class="card-counter">X</span></a>
+    <a class="nav-link active" href="#">Shows <span class="card-counter"></span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Actors <span class="card-counter">X</span></a>
+    <a class="nav-link" href="#">Actors <span class="card-counter"></span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Actresses <span class="card-counter">X</span></a>
+    <a class="nav-link" href="#">Actresses <span class="card-counter"></span></a>
   </li>
 </ul>
 </header>
@@ -31,6 +31,22 @@ const render = () => {
   content.insertAdjacentHTML('beforeend', footer);
 };
 
+const getCount = (section) => {
+  let content;
+  switch (section) {
+    case 0:
+      content = document.getElementById('shows');
+      break;
+    case 1:
+      content = document.getElementById('actors');
+      break;
+    case 2:
+      content = document.getElementById('actoras');
+      break;
+  }
+  return content.childElementCount;
+}
+
 const listeners = () => {
   const showsSection = document.getElementById('shows');
   const actorsSection = document.getElementById('actors');
@@ -39,15 +55,18 @@ const listeners = () => {
   const counters = document.querySelectorAll('.card-counter');
 
   navLinks[0].addEventListener('click', (e) => {
+    const count = getCount(0);
     showsSection.style.display = 'grid';
     actorsSection.style.display = 'none';
     actorasSection.style.display = 'none';
     counters[0].style.display = 'inline';
     counters[1].style.display = 'none';
     counters[2].style.display = 'none';
+    counters[0].innerHTML = `(${count})`;
   });
 
   navLinks[1].addEventListener('click', (e) => {
+    const count = getCount(1);
     showsSection.style.display = 'none';
     actorsSection.style.display = 'grid';
     actorasSection.style.display = 'none';
@@ -57,6 +76,7 @@ const listeners = () => {
   });
 
   navLinks[2].addEventListener('click', (e) => {
+    const count = getCount(2);
     showsSection.style.display = 'none';
     actorsSection.style.display = 'none';
     actorasSection.style.display = 'grid';
