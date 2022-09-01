1126dab0685581f95adf91c4e4e8fd8f5d9a39fd
Lucas Comamala <lucascomamala@gmail.com>
2022-09-01 12:26:28 -0600

    Add counters HTML

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -7,13 +7,13 @@ const header = `
     <img src="" class="logo" alt="main logo" />
   </li>
   <li class="nav-item">
-    <a class="nav-link active" href="#">Shows</a>
+    <a class="nav-link active" href="#">Shows <span class="card-counter">X</span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Actors</a>
+    <a class="nav-link" href="#">Actors <span class="card-counter">X</span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Actresses</a>
+    <a class="nav-link" href="#">Actresses <span class="card-counter">X</span></a>
   </li>
 </ul>
 </header>
@@ -36,23 +36,33 @@ const listeners = () => {
   const actorsSection = document.getElementById('actors');
   const actorasSection = document.getElementById('actoras');
   const navLinks = document.querySelectorAll('.nav-link');
+  const counters = document.querySelectorAll('.card-counter');
 
-  navLinks[0].addEventListener('click', () => {
+  navLinks[0].addEventListener('click', (e) => {
     showsSection.style.display = 'grid';
     actorsSection.style.display = 'none';
     actorasSection.style.display = 'none';
+    counters[0].style.display = 'inline';
+    counters[1].style.display = 'none';
+    counters[2].style.display = 'none';
   });
 
-  navLinks[1].addEventListener('click', () => {
+  navLinks[1].addEventListener('click', (e) => {
     showsSection.style.display = 'none';
     actorsSection.style.display = 'grid';
     actorasSection.style.display = 'none';
+    counters[0].style.display = 'none';
+    counters[1].style.display = 'inline';
+    counters[2].style.display = 'none';
   });
 
-  navLinks[2].addEventListener('click', () => {
+  navLinks[2].addEventListener('click', (e) => {
     showsSection.style.display = 'none';
     actorsSection.style.display = 'none';
     actorasSection.style.display = 'grid';
+    counters[0].style.display = 'none';
+    counters[1].style.display = 'none';
+    counters[2].style.display = 'inline';
   });
 };
 
