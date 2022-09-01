13fe1b2c9fb41c5c5d6b930581891f1a726b95d5
Lucas Comamala <lucascomamala@gmail.com>
2022-09-01 14:38:05 -0600

    Add test

--- src/modules/__tests__/nav.test.js
+++ src/modules/__tests__/nav.test.js
@@ -6,5 +6,8 @@ import { displayCount } from '../nav.js';
 import mockNav from '../__mocks__/nav.mock.js';
 
 test('Add 9 items and check count', () => {
-
+  mockNav();
+  displayCount(0);
+  const list = document.querySelectorAll('.card-counter');
+  expect(list).toHaveLength(9);
 });
