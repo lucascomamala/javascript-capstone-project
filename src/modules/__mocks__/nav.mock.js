d5f3be1ebf109b6549d90c4c07bbc011fc088fb3
Lucas Comamala <lucascomamala@gmail.com>
2022-09-01 14:54:36 -0600

    Remove unnecessary loop in mock

--- src/modules/__mocks__/nav.mock.js
+++ src/modules/__mocks__/nav.mock.js
@@ -1,16 +1,16 @@
 const mockNav = (x) => {
   const content = document.createElement('section');
-  for (let i = 0; i < 3; i += 1) {
-    const counter = document.createElement('div');
-    counter.classList.add('card-counter');
-    document.body.appendChild(counter);
-  }
+  const counter = document.createElement('div');
+  counter.classList.add('card-counter');
+
   for (let i = 0; i < x; i += 1) {
     const child = document.createElement('div');
     child.classList.add('show');
     child.innerHTML = '<div><ul class="card"></li></div>';
     content.appendChild(child);
   }
+
+  document.body.appendChild(counter);
   document.body.appendChild(content);
 };
 
